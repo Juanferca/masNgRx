1d0722d949e7d89fd6c66a9a9854baec1322fe61
Juanferca <juanferca@hotmail.com>
2019-04-19 18:41:20 +0200

    Primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBgAjya5Zeg2xJj2F7hfrgQHF0JUk0kFfU',
+    authDomain: 'ingreso-egreso-app-2de53.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-2de53.firebaseio.com',
+    projectId: 'ingreso-egreso-app-2de53',
+    storageBucket: 'ingreso-egreso-app-2de53.appspot.com',
+    messagingSenderId: '317918486763'
+   }
 };
